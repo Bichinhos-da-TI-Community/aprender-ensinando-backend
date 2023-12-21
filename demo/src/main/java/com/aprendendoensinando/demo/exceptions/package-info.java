a1bf1487ef41d6a52e4f2984ec328b86fbcbf029
jonael.santos <jonael.santos@tecnogroup.com.br>
2023-12-21 12:30:05 -0300

    FEAT: Inclusao de package.info

---
+++ demo/src/main/java/com/aprendendoensinando/demo/exceptions/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Classes necessárias para manipular retornos de exceptions personalizadas.
+ * 
+ * @author Jonael Santos
+ * @version $I$, $G$
+ * @since 1.00
+ */
+
+package com.aprendendoensinando.demo.exceptions;
---
+++ demo/src/main/java/com/aprendendoensinando/demo/security/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Classes necessárias para manipular dados relativos a autenticação e criptografia de dados.
+ * 
+ * @author Jonael Santos
+ * @version $I$, $G$
+ * @since 1.00
+ */
+
+package com.aprendendoensinando.demo.security;
