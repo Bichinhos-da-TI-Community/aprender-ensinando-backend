babd84852bd1303e5b503a6e726fcdb2f52b3e67
Otavio Marques <marquesotavio251204@gmail.com>
2023-12-20 22:55:11 -0300

    Configurado application.properties

--- demo/src/main/java/com/aprendendoensinando/demo/DemoApplication.java
+++ demo/src/main/java/com/aprendendoensinando/demo/DemoApplication.java
@@ -1,13 +1,14 @@
 package com.aprendendoensinando.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 
 }
